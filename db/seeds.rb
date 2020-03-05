c7479ffcafa044650b29a3d6b34bf77265c3bbfd
JohnZZZ86 <johnzzz@mail.ru>
2020-03-05 11:48:56 +0300

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-#store_list = ["ALMI", "EuroOpt", "Korona", "Green"]
-#store_list.each do |name|
-#  Store.create!(name: name)
-#end
+store_list = ["ALMI", "EuroOpt", "Korona", "Green"]
+store_list.each do |name|
+  Store.create!(name: name)
+end
 
-#item_list = [
-#    [ "XL", "Shirt" , "Red" ],
-#    [ "L", "Boots" , "Black" ],
-#    [ "XS", "Socks" , "White" ],
-#    [ "XXL", "Pants" , "Blue" ],
-#    [ "XXS", "Jacket" , "Green" ],
-#    [ "M", "Hat" , "Brown" ],
-#    [ "XXXL", "Boots" , "Red" ],
-#]
-#item_list.each do |size, name, color|
-#  Item.create(size: size, name: name, color: color)
-#end
+item_list = [
+    [ "XL", "Shirt" , "Red" ],
+    [ "L", "Boots" , "Black" ],
+    [ "XS", "Socks" , "White" ],
+    [ "XXL", "Pants" , "Blue" ],
+    [ "XXS", "Jacket" , "Green" ],
+    [ "M", "Hat" , "Brown" ],
+    [ "XXXL", "Boots" , "Red" ],
+]
+item_list.each do |size, name, color|
+  Item.create(size: size, name: name, color: color)
+end
 
 store_item_list = [
     [10, 6, 1],
