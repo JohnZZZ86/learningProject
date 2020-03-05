7bb11742ebeaa63f0ce2b8d0ee1e517dfb67fe9a
JohnZZZ86 <johnzzz@mail.ru>
2020-03-05 17:35:46 +0300

    Update add_data_to_items migration and added migration to populate store_items table DB

--- db/migrate/20200305090642_add_data_to_items.rb
+++ db/migrate/20200305090642_add_data_to_items.rb
@@ -1,24 +1,16 @@
 class AddDataToItems < ActiveRecord::Migration[6.0]
-  def find_curent_element_in_db
-
-  end
-
   def get_rand_item
-    clothes = ["Shirt", "Pullover", "Socks", "Pants", "Jacket", "Hat", "Cap"]
-    sizes = ["XXXS","XXS", "XS", "S", "M","L", "XL", "XXL","XXXL"]
-    colors = ["Red", "Black", "White", "Blue", "Green", "Brown"]
+    CLOTHES = ["Shirt", "Pullover", "Socks", "Pants", "Jacket", "Hat", "Cap"]
+    SIZES = ["XXXS","XXS", "XS", "S", "M","L", "XL", "XXL","XXXL"]
+    COLORS = ["Red", "Black", "White", "Blue", "Green", "Brown"]
 
     result_items = []
     index = 0
-    clothes.each do |clothe|
-      sizes.each do |size|
-        colors.each do |color|
-          if Item.where(["name = ? and size = ? and color = ?",clothe, size, color]).empty?
-            temp_item = []
-            temp_item[0] = clothe
-            temp_item[1] = size
-            temp_item[2] = color
-            result_items[index] = temp_item
+    CLOTHES.each do |clothe|
+      SIZES.each do |size|
+        COLORS.each do |color|
+          if Item.exists("name = ? and size = ? and color = ?",clothe, size, color).empty?
+            result_items << [ clothe, size, color ]
             index += 1
           end
         end
