9a951a5656477a1bdabecad9372ce3a9c2d8fab8
JohnZZZ86 <johnzzz@mail.ru>
2020-03-11 14:45:05 +0300

    Update script to populate store_items

--- lib/get_unique_values_by_given_count.rb
+++ lib/get_unique_values_by_given_count.rb
@@ -1,18 +1,4 @@
 class GetUniqueValuesByGivenCount
-  require 'active_record'
-  require_relative '../app/models/application_record'
-  require_relative '../app/models/store'
-  require_relative '../app/models/store_item'
-  require_relative '../app/models/item'
-
-  ActiveRecord::Base.establish_connection(
-      :adapter => "mysql2",
-      :host => "localhost",
-      :database => "Learning",
-      :username => "root",
-      :password => "root"
-  )
-
   COUNT_OF_VALUES_RANDOM_ARRAY = 400
 
   def initialize()
@@ -21,22 +7,23 @@ class GetUniqueValuesByGivenCount
 
   def generate_original_array
     original_array = []
+    item_ids_list = Item.pluck(:id)
     (1..COUNT_OF_VALUES_RANDOM_ARRAY).each do
-      original_array << rand(Item.minimum(:id)..Item.maximum(:id))
+      original_array << item_ids_list[rand(0..item_ids_list.length-1)]
     end
-    puts "Random generated array - " + original_array.join
+    #puts "Random generated array - " + original_array.join
     original_array
   end
 
   def get_uniq_array_given_length(temp_array)
-    unless temp_array.length < 1 || @output_array.length > COUNT_OF_VALUES_RANDOM_ARRAY - 1
+    unless temp_array.length < 1 || @output_array.length > @output_array_length - 1
       index = rand(0..temp_array.length-1)
       if @output_array.include?(temp_array[index])
         temp_array.delete_at(index)
       else
         @output_array << temp_array[index]
         temp_array.delete_at(index)
-        puts  " index = " + index.to_s  + " " + @output_array.join.to_s + " " + temp_array.join.to_s
+        #puts  " index = " + index.to_s  + " " + @output_array.join.to_s + " " + temp_array.join.to_s
         get_uniq_array_given_length(temp_array)
         @output_array
       end
--- my_test.rb
+++ my_test.rb
@@ -1,4 +1,3 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__), "lib")
 require 'get_unique_values_by_given_count.rb'
 
 GetUniqueValuesByGivenCount.new.output_stores
