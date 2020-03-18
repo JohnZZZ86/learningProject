31d03ee84002e0ec8dfc63910f3ca5208656f72e
JohnZZZ86 <johnzzz@mail.ru>
2020-03-18 11:00:41 +0300

    init

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require_relative 'boot'
 require 'rails/all'
+require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module Learning
   class Application < Rails::Application
     config.load_defaults 6.0
     config.autoload_paths << Rails.root.join("lib")
+    config.assets.initialize_on_precompile = false
   end
 end
